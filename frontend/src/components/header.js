4e69ffedf296cff5f657962e5c2a649839aa266e
deepapaikar <deepapaikar19@gmail.com~>
2023-11-14 16:46:57 -0500

    ui development

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -5,7 +5,7 @@ class Header extends React.Component
     {
         return(
             <div style={{backgroundColor:'skyblue',width:'100%',height:'10%'}}>
-                <h1 style={{padding:'7px',margin:'0px',fontFamily:'cursive'}}>MY CHAT BOT</h1>
+                <h1 style={{padding:'7px',margin:'0px',fontFamily:'cursive'}}>MY CODE GEN CHATBOT</h1>
             </div>
         );
     }
