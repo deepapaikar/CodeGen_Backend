7295db0459f8c073d92f129e0c257d5a9729288a
lpxgita <32610337+lpxgita@users.noreply.github.com>
2023-12-05 15:38:16 -0500

    fix response form

--- backend/autogen_modifi/autogen/agentchat/conversable_agent.py
+++ backend/autogen_modifi/autogen/agentchat/conversable_agent.py
@@ -407,14 +407,17 @@ class ConversableAgent(Agent):
         # print the message received
         print(colored(sender.name, "yellow"), "(to", f"{self.name}):\n", flush=True)
         message = self._message_to_dict(message)
-
+        res = ""
         if message.get("role") == "function":
             func_print = f"***** Response from calling function \"{message['name']}\" *****"
             print(colored(func_print, "green"), flush=True)
             print(message["content"], flush=True)
             print(colored("*" * len(func_print), "green"), flush=True)
+            res+=func_print
+            res += message["content"]
         else:
             content = message.get("content")
+            res += content_str(content)
             if content is not None:
                 if "context" in message:
                     content = OpenAIWrapper.instantiate(
@@ -437,7 +440,7 @@ class ConversableAgent(Agent):
                 )
                 print(colored("*" * len(func_print), "green"), flush=True)
         if self.socket_room_id is not None:
-            emit('message', {'sender':sender.name,'data':message}, room=self.socket_room_id)
+            emit('message', {'sender':sender.name,'data':res}, room=self.socket_room_id)
         print("\n", "-" * 80, flush=True, sep="")
 
     def _process_received_message(self, message: Union[Dict, str], sender: Agent, silent: bool):
